0bf114b10b2cc7c8b84d58d0551ab513db91f7d8
Benedict Yappy <yappy.benedict@gmail.com>
2020-04-30 15:22:34 +0700

    Updates

---
+++ precache-manifest.3ea46ac3cf7d5a77e57b4a18a7f985ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e577248558943528dd3cdf03e56f9b",
+    "url": "/follow-react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8e53414f3dd5b62aec7d",
+    "url": "/follow-react-tic-tac-toe/static/css/main.2e63c7e8.chunk.css"
+  },
+  {
+    "revision": "6ceee1df5e0f8256bf62",
+    "url": "/follow-react-tic-tac-toe/static/js/2.ad88b885.chunk.js"
+  },
+  {
+    "revision": "8e53414f3dd5b62aec7d",
+    "url": "/follow-react-tic-tac-toe/static/js/main.4730da80.chunk.js"
+  },
+  {
+    "revision": "695d615481d2afa4d834",
+    "url": "/follow-react-tic-tac-toe/static/js/runtime~main.23e24035.js"
+  }
+]);
